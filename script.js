03673954c675ee2f9e6806779c3e796fb4631daf
david-bol12 <davidboland1001@gmail.com>
2025-03-02 22:02:08 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function hover(element, id) {
+  element.setAttribute('src', 'Assets/Social Media Icons/Highlight/'+ id +'.svg');
+}
+
+function unhover(element, id) {
+  element.setAttribute('src', 'Assets/Social Media Icons/Grey/'+ id +'.svg');
+}
